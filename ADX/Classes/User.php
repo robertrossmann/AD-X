45c96d45c8129d1d01e196ec7b30d3337e912a0e
Anders K. Madsen <lillesvin@users.noreply.github.com>
2016-06-06 10:59:39 +0200

    Fix 'link' property reference in User class

--- ADX/Classes/User.php
+++ ADX/Classes/User.php
@@ -426,7 +426,7 @@ class User extends Object
 		if ( count( $diff ) > 0 ) throw new Core\IncorrectParameterException( "Mandatory attribute '" . implode( "', '", $diff ) . "' is missing" );
 
 		// Generate UserPrincipalName
-		$domain = ldap_explode_dn( $link->rootDSE->defaultNamingContext(0), 1 );
+		$domain = ldap_explode_dn( $this->adxLink->rootDSE->defaultNamingContext(0), 1 );
 		unset( $domain['count'] );
 		$domain = implode( '.', $domain );
 
